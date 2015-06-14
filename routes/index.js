cc655c487e4d513998e501f28714f41c9e89fab2
Alex Shestmintsev <shestmintsev@gmail.com>
2015-06-14 15:00:14 +0300

    refactor

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,10 @@ var gcmapParser = require('gcmap-parser');
 var Airports = require('../models/Airports');
 
 
-
-
-
 exports.index = function(req, res){
   Airports.getSavedObjects(function(data){
     res.render('index', { title: 'Express', airports: data.reverse() });
   });
-
 };
 
 exports.delete = function(req, res){
@@ -17,24 +13,20 @@ exports.delete = function(req, res){
   Airports.deleteByKey(key,function(){
     res.redirect('/');
   });
-
 };
 
 exports.update = function(req, res){
   Airports.UpdateByKey(req.body,function(){
     res.redirect('/');
   });
-
 };
 
 exports.indexpost = function(req, res){
   var key = req.body.key;
   gcmapParser.parse(key,function(data){
-
     Airports.saveNewObj(key,data, function(airports){
       res.render('index', { title: 'Express', airports: airports.reverse() });
     });
-
   });
 
 };
